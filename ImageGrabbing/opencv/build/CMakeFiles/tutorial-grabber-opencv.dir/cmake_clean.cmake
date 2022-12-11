e75a8e4bf8d8a3282c63de3b7a6e4f0e9465095c
Xicheng-Uni-Stuttgart <whhithxc@gmail.com>
2022-12-11 16:17:59 +0100

    add some files

---
+++ ImageGrabbing/opencv/build/CMakeFiles/tutorial-grabber-opencv.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tutorial-grabber-opencv.dir/tutorial-grabber-opencv.cpp.o"
+  "tutorial-grabber-opencv"
+  "tutorial-grabber-opencv.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tutorial-grabber-opencv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
