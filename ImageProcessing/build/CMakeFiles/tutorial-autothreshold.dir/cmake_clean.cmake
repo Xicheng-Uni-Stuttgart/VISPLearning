a72836f808c40b5da13bf177277ffd1d3b9d5df0
Xicheng-Uni-Stuttgart <whhithxc@gmail.com>
2022-12-11 22:12:19 +0100

    add ImageProcessing

---
+++ ImageProcessing/build/CMakeFiles/tutorial-autothreshold.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tutorial-autothreshold.dir/tutorial-autothreshold.cpp.o"
+  "tutorial-autothreshold"
+  "tutorial-autothreshold.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tutorial-autothreshold.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ImageProcessing/build/CMakeFiles/tutorial-contour.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tutorial-contour.dir/tutorial-contour.cpp.o"
+  "tutorial-contour"
+  "tutorial-contour.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tutorial-contour.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
